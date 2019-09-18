ff2a033acb1323a533971a94a6927986bea32ae7
garput2 <55056069+garput2@users.noreply.github.com>
2019-09-18 08:01:23 +0700

    Create angkapositif2.py

---
+++ angkapositif2.py
@@ -0,0 +1,10 @@
+print("Masukkan nilai N:", end=" ")
+N = int(input())
+
+if (N>=0):
+    if(N>0):
+        print("Bilangan positif")
+    else:
+        print("Nol")
+else:
+    print("Bilangan negatif")
