6f40c038bdb947f995178ac60becc99fa4501047
garput2 <55056069+garput2@users.noreply.github.com>
2019-09-18 08:02:22 +0700

    Create kelas1.py

---
+++ kelas1.py
@@ -0,0 +1,10 @@
+sebuahinteger = 1
+sebuahboolean = True
+print(sebuahinteger)
+def afunction(): #afunction adalah fungsi yang mengembalikan nilai 1
+    return 1
+print (afunction())
+listofint = [1,2,3,4,5,6,7] #mendefinisikan list
+for x in 1,2,3: 
+    if x in listofint: #jika konten x adalah anggota listofint, print true
+        print(sebuahboolean)
